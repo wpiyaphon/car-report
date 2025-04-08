7b28210d4ba6c4f329b84254498edf4a5a1217fe
Piyaphon Wu (Hong) <wupiyaphon@gmail.com>
2025-04-08 20:33:43 +0700

    add shadcn and husky with scripts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,6 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <Button variant="outline">Click me</Button>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
